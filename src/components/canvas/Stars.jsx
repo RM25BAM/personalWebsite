170db9d9b16cf790eb71d97235ed25eabc3b0871
Natasha Piedrabuena <natashapiedrabuena@Natashas-MacBook-Pro.local>
2024-04-16 16:27:46 -0400

    missing

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -6,7 +6,7 @@ import * as random from 'maath/random/dist/maath-random.esm';
 
 const Stars = (props) => {
   const ref = useRef();
-  const sphere = random.inSphere(new Float32Array(20000), { radius: 2.2 })
+  const sphere = random.inSphere(new Float32Array(40000), { radius: 2.2 })
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
